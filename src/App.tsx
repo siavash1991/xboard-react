9bf771185221b5dd5b67b4874dcac746aeebf12b
Siavash Rahimi Azad <siavash.rahimi1991@gmail.com>
2024-02-06 21:04:27 +0300

    Add react-router-dom and redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;

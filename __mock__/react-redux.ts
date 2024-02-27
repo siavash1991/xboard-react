a5967734253e21690ff57f8cbebf3c1d9956996e
siavash1991 <siavash1991@users.noreply.github.com>
2024-02-27 21:28:12 +0000

    deploy: 7d0ef5c6e000ef63b104443b5be1e7030989d1f4

--- __mock__/react-redux.ts
+++
@@ -1,6 +0,0 @@
-// __mocks__/react-redux.ts
-
-import { Dispatch } from 'redux';
-
-export const useDispatch = jest.fn<Dispatch<any>, []>();
-export const useSelector = jest.fn();
--- src/components/atoms/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-const XLoadingSpinner: React.FC = () => (
-	<div className="spinner-container">
-		{/* You can replace this with your preferred loading animation or library */}
-		<div className="spinner"></div>
-	</div>
-);
-export default XLoadingSpinner;

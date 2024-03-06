b6d6f5a4d479e90efa10af83cd539c833603f9ca
Siavash Rahimi Azad <siavash.rahimi1991@gmail.com>
2024-03-06 17:10:46 +0300

    feat: Implement custom dark mode toggle functionality
    Implemented custom dark mode toggle functionality in the XHeader component. Removed dependency on Flowbite for dark mode toggling and instead used a custom solution. This allows for more flexibility and customization in the dark mode toggle behavior.

---
+++ cypress/e2e/darkmode-toggle.cy.ts
@@ -0,0 +1,38 @@
+describe('Dark Mode Toggle Button', () => {
+	it('Toggles dark mode when clicked', () => {
+		// Visit the page containing the dark mode toggle button
+		cy.visit('https://siavash1991.github.io/xboard-react/#/');
+
+		// Verify that dark mode is initially disabled
+		cy.get('html').should('not.have.class', 'dark');
+
+		// Click on the dark mode toggle button
+		cy.get('[data-testid=dark-theme-toggle]').click();
+
+		// Verify that dark mode is enabled after clicking
+		cy.get('html').should('have.class', 'dark');
+
+		// Click on the dark mode toggle button again
+		cy.get('[data-testid=dark-theme-toggle]').click();
+
+		// Verify that dark mode is disabled after clicking again
+		cy.get('html').should('not.have.class', 'dark');
+	});
+
+	it('Preserves dark mode state if set before the test', () => {
+		// Set dark mode manually before visiting the page
+		localStorage.setItem('darkMode', 'true');
+
+		// Visit the page containing the dark mode toggle button
+		cy.visit('https://siavash1991.github.io/xboard-react/#/');
+
+		// Verify that dark mode is enabled
+		cy.get('html').should('have.class', 'dark');
+
+		// Click on the dark mode toggle button
+		cy.get('[data-testid=dark-theme-toggle]').click();
+
+		// Verify that dark mode is disabled after clicking
+		cy.get('html').should('not.have.class', 'dark');
+	});
+});

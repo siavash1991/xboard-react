a5e82b3ffb6a33a41edc0ff7122bf2704bd3041d
Siavash Rahimi Azad <siavash.rahimi1991@gmail.com>
2024-03-06 12:46:56 +0300

    Configure Cypress and add test for homepage

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,9 @@
+describe('Home Page Test', () => {
+	it('Visits the home page', () => {
+		// Visit the root URL of GitHub Pages
+		cy.visit('https://siavash1991.github.io/xboard-react/#/');
+
+		// Assert that the home page is loaded
+		cy.contains('Dashboard'); // Replace 'Home Page Title' with the title or content of your home page
+	});
+});
